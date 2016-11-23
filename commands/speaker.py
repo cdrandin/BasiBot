7130b4e11cff59787dd8afb094e7080ac52b02a7
Brandon Forbes <bforbes@gaikai.com>
2016-11-23 12:05:18 -0800

    Created Images folder

--- commands/speaker.py
+++ commands/speaker.py
@@ -1,7 +1,7 @@
 async def speaker(client, message):
     print("Upload Received")
     try:
-        await client.send_file(message.channel, "speaker.png")
+        await client.send_file(message.channel, "images/speaker.png")
     except Exception as e:
         await client.send_message(message.channel, "ERROR: %s" % e)
     return
